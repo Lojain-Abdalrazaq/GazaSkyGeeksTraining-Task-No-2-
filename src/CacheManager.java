1341d525cc1b7ff5e52b66a3856d29a4e0665f8b
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-07-23 22:48:53 +0300

    commit

--- src/CacheManager.java
+++ src/CacheManager.java
@@ -1,13 +1,50 @@
 import java.util.HashMap;
-import java.util.Map
+import java.util.Map;
 
 public class CacheManager {
+
+    // variable to hold the instance of the cache manager
     private static CacheManager cache_instance;
+    // the map of the cache
     private Map<String,String> cache;
 
     private CacheManager(){
         cache = new HashMap<>();
     }
-
+    // getting the instance if exists or creating the instance in case of first time calling the function
+    public static CacheManager getCacheInstance(){
+        System.out.println("=============================================");
+        if(cache_instance == null){
+            synchronized (CacheManager.class) {
+                if (cache_instance == null) {
+                    System.out.println("The instance is created....");
+                    cache_instance = new CacheManager();
+                }
+            }
+        }
+        return cache_instance;
+    }
+    // function to add on the cache
+    public void addToCache(String key, String value){
+        System.out.println("=============================================");
+        System.out.println("Adding to cache "+ key+ " " + value);
+        cache.put(key, value);
+        System.out.println("Added to cache sucessfully!!");
+    }
+    // function for getting item from the cache
+    public void getItem(String key){
+        System.out.println("=============================================");
+        System.out.println(cache.get(key));
+    }
+    // function to check if the item exits in the cache using the key
+    public boolean containsItem(String key){
+        System.out.println("=============================================");
+        if (cache.containsKey(key)){
+            System.out.println("Yes! The cache contains the item with key" +key +" and the value is " + cache.get(key));
+            return true;
+        }
+        System.out.println("No. The cache does not contain and item with key "+key);
+        return false;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Lojain Abdalrazaq - Task No.2
+ *
+ * In my task, I used the concept of cache manager as an example and an application of Singleton design pattern. In cache manager application,
+ * we need only one instance of the cache in order to use it in saving , reading or retreving items on it, and the singleton patterns solves the problem.
+ * Using the singleton pattern ensures that we have one cache accessable among the application and ovoide to have multiple instances of caches
+ * which could be resource-intensive.
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // firstly, we want to get the singleton instance of te cache manager
+        CacheManager cacheManager =  CacheManager.getCacheInstance();
+
+        // adding items to the cache map using the addToCache function
+        cacheManager.addToCache("user1", "Lojain Abdalrazaq");
+        cacheManager.addToCache("user2", "ahmad mahmoud");
+
+        // accessing data from the cache using getItem function
+        cacheManager.getItem("user1");
+
+        // checking of the item is exists in the cache or not using containsItem function
+        cacheManager.containsItem("user2");
+        cacheManager.containsItem("user10");
     }
 }
